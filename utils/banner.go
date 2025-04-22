1a28fb689f526bfd1bf2097a3b216e2fa1436dd1
s-0-u-l-z <ttvpro2612@gmail.com>
2025-04-21 21:57:00 -0400

    Update banner.go
    Dynamic Logo

--- utils/banner.go
+++ utils/banner.go
@@ -2,13 +2,19 @@ package utils
 
 import (
 	"fmt"
+
 	"github.com/fatih/color"
 )
 
-func PrintBanner(version string) {
+func PrintBanner(version, wordlist string, threads int, mode string) {
 	darkBlue := color.New(color.FgBlue).SprintFunc()
 	lightCyan := color.New(color.FgCyan).SprintFunc()
 	yellow := color.New(color.FgYellow).SprintFunc()
+
+	wordlistLabel := "Auto"
+	if wordlist != "" && wordlist != "wordlist.txt" {
+		wordlistLabel = wordlist
+	}
 
 	fmt.Println(darkBlue(`  e88~~\           888~-_               d8   888      `))
 	fmt.Println(darkBlue(` d888      e88~-_  888   \    /~~~8e  _d88__ 888-~88e `))
@@ -17,7 +23,6 @@ func PrintBanner(version string) {
 	fmt.Println(darkBlue(` Y888   | Y888   ' 888_-~   C888  888  888   888  888 `))
 	fmt.Println(darkBlue(`  "88__/   "88_-~  888       "88_-888  "88_/ 888  888 `))
 	fmt.Println(lightCyan("------------------------------------------------------------"))
-	fmt.Println(yellow("▶ Mode: Standard   ▶ HTTP method: GET   ▶ Threads: 25   ▶ Wordlist: Auto"))
+	fmt.Println(yellow(fmt.Sprintf("▶ Mode: %s   ▶ HTTP method: GET   ▶ Threads: %d   ▶ Wordlist: %s", mode, threads, wordlistLabel)))
 	fmt.Println(lightCyan("------------------------------------------------------------\n"))
 }
-
