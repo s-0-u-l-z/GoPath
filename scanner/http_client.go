98706c9b7ec6832913d9005ae237ef2e48a54769
s-0-u-l-z <ttvpro2612@gmail.com>
2025-04-21 22:09:07 -0400

    Update http_client.go

--- scanner/http_client.go
+++ scanner/http_client.go
@@ -3,13 +3,12 @@ package scanner
 import (
 	"net/http"
 	"time"
-	
 )
 
-var client = &http.client{
-	Timeout:5 * time.Second,
+var client = &http.Client{
+	Timeout: 5 * time.Second,
 	Transport: &http.Transport{
-		MaxIdleConns:	 100,
+		MaxIdleConns:        100,
 		MaxIdleConnsPerHost: 100,
 		IdleConnTimeout:     90 * time.Second,
 	},
